0c7208617e4afa786bb41d2a14a49034edd1a679
Prince Ita <princeIta@outlook.com>
2021-06-20 12:51:06 +0100

    add email provider, nodemailer

--- src/datasources/gateway/messaging.gateway.ts
+++ src/datasources/gateway/messaging.gateway.ts
@@ -1,27 +1,69 @@
-import { ScheduledMessage } from '../../models/scheduled-message';
+import {
+  IScheduledRecipient,
+  ScheduledMessage
+} from '../../models/scheduled-message';
+import { IMessage } from '../../models/logged-message';
 
 export interface IMessagingProvider {
   email: any;
   sms: any;
 }
+// B8A2DB44233D995E5A84EB05449DC78A751841D67C1DB057D40E835A2D88FC05F8E8DF4102B14166ECC68682F867FB8F
 
-function sendSms(message: any, provider: any) {}
+function sendSms(message: IMessage | undefined, provider: any) { }
 
-function sendEmail(message: any, provider: any) {}
+async function sendEmail<P extends IMessagingProvider>(
+  message: IMessage | undefined,
+  recipients: Array<IScheduledRecipient> | undefined,
+  provider: P
+) {
+  const emailProvider = provider.email
+  if (recipients?.length) {
+    for await (let recipient of recipients) {
+      let testAccount = await emailProvider.createTestAccount();
 
-export default class MessagingGateway {
-  private messagingProvider: IMessagingProvider;
+      let transporter = emailProvider.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: testAccount.user, // generated ethereal user
+          pass: testAccount.pass // generated ethereal password
+        }
+      });
 
-  constructor(messagingProvider: IMessagingProvider) {
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: '"Hermis 👻" <princeIta@outlook.com>', // sender address
+        to: recipient.email, // list of receivers
+        subject: message?.subject, // Subject line
+        text: message?.content, // plain text body
+      });
+
+      console.log('Message sent: %s', info.messageId);
+
+      console.log('Preview URL: %s', emailProvider.getTestMessageUrl(info));
+    }
+  }
+}
+
+export default class MessagingGateway<P extends IMessagingProvider> {
+  private messagingProvider: P;
+
+  constructor(messagingProvider: P) {
     this.messagingProvider = messagingProvider;
   }
 
-  async send(message: ScheduledMessage) {
-    if (message.isMessageTypeEmail()) {
-      return sendEmail(message, this.messagingProvider);
+  async send(scheduledMessage: ScheduledMessage) {
+    if (scheduledMessage.isMessageTypeEmail()) {
+      return sendEmail(
+        scheduledMessage.message,
+        scheduledMessage.recipients,
+        this.messagingProvider
+      );
     } else {
-      if (message.isMessageTypeSms()) {
-        return sendSms(message, this.messagingProvider);
+      if (scheduledMessage.isMessageTypeSms()) {
+        return sendSms(scheduledMessage.message, this.messagingProvider);
       }
     }
   }
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,9 @@
 import cron from 'node-cron';
+import nodemailer from "nodemailer"
 
 import AuthService from './auth.service';
 import ClientService from './client.service';
-import ScheduledMessageService from './scheduled-message.service';
+import ScheduleMessageService from './schedule-message.service';
 import Database from '../datasources/db/models';
 import MessagingService from './messaging.service';
 import { ICronProvider } from '../datasources/gateway/jobs.gateway';
@@ -10,20 +11,20 @@ import { IMessagingProvider } from '../datasources/gateway/messaging.gateway';
 
 export const authService = new AuthService();
 export const clientService = new ClientService(Database);
-export const scheduledMessageService = new ScheduledMessageService(
+export const scheduledMessageService = new ScheduleMessageService(
   Database,
-  { email: {}, sms: {} },
+  { email: nodemailer, sms: {} },
   cron
 );
 export const messagingService = new MessagingService(Database, {
-  email: {},
+  email: nodemailer,
   sms: {}
 });
 
 export interface IServices {
   authService: AuthService;
   clientService: ClientService;
-  scheduledMessageService: ScheduledMessageService<
+  scheduledMessageService: ScheduleMessageService<
     ICronProvider,
     IMessagingProvider
   >;
